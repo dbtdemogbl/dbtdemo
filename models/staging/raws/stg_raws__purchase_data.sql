3fbe08958406eae420c765011fe3536fad7fbd66
dbt.demo.gbl@gmail.com <dbt.demo.gbl@gmail.com>
2023-08-21 08:08:49 +0000

    commit

---
+++ models/staging/raws/stg_raws__purchase_data.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('raws', 'purchase_data') }}
+
+),
+
+renamed as (
+
+    select
+        purchase_id,
+        customer_id,
+        purchase_date,
+        product_id,
+        quantity
+
+    from source
+
+)
+
+select * from renamed
